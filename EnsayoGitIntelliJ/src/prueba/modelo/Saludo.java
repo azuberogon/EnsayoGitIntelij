9b2705819a63737c049e0efed14ee755f67471c7
azuberogon <azuberogondam1b@gmail.com>
2023-04-08 13:20:53 +0200

    1º commit ensayo

---
+++ EnsayoGitIntelliJ/src/prueba/modelo/Saludo.java
@@ -0,0 +1,21 @@
+package prueba.modelo;
+
+/**
+ * Modela un saludo personalizado
+ * 
+ * 
+ */
+
+public class Saludo {
+
+	 
+	public Saludo() {
+
+	}
+ 
+	public String  saludar(String nombre) {
+
+		return "Hi " + nombre;
+
+	}
+}
