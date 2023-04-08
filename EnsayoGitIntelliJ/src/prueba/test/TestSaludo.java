b0847ec1f0dc27d1eeb4bc7896bb411541dd282f
AntonioDam1 <antonioaguidam1@gmail.com>
2023-04-08 18:00:32 +0200

    Probando

--- EnsayoGitIntelliJ/src/prueba/test/TestSaludo.java
+++ EnsayoGitIntelliJ/src/prueba/test/TestSaludo.java
@@ -8,6 +8,7 @@ public class TestSaludo {
 		Saludo saludo = new Saludo();
 		System.out.println(saludo.saludar("Juan ",14));
 		System.out.println(saludo.saludar("Ana ",15));
+		System.out.println(saludo.saludar("Federico ",17));
 		 
 	}
 
