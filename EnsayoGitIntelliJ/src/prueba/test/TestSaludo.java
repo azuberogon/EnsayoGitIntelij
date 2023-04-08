24a21b44cdf04cd534404899c7566f480a34a99c
AntonioDam1 <antonioaguidam1@gmail.com>
2023-04-08 17:38:21 +0200

    Segundo commit Colaborador

--- EnsayoGitIntelliJ/src/prueba/test/TestSaludo.java
+++ EnsayoGitIntelliJ/src/prueba/test/TestSaludo.java
@@ -6,7 +6,8 @@ public class TestSaludo {
 
 	public static void main(String[] args) {
 		Saludo saludo = new Saludo();
-		System.out.println(saludo.saludar("juan ",14));
+		System.out.println(saludo.saludar("Juan ",14));
+		System.out.println(saludo.saludar("Ana ",15));
 		 
 	}
 
